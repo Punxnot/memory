6d7f110d4c1d1633187fddb9339d72f95320b1db
Marina <do-you-speak@yandex.ru>
2016-03-13 16:42:32 +0300

    Add bot

--- all.js
+++ all.js
@@ -1,6 +1,5 @@
 (function() {
-  var allCards, blub, card1, card2, cardsContainer, createCards, endList, exposed, i, j, len, list1, list2, messageContainer, newList, shuffle, state, turns, yeah,
-    indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+  var allCards, blub, card1, card2, cardsContainer, createCards, endList, exposed, getRandom, i, j, len, list1, list2, messageContainer, myInterval, newList, playersTurn, shuffle, state, turns, yeah;
 
   list1 = [0, 1, 2, 3, 4, 5, 6, 7];
 
@@ -33,6 +32,10 @@
 
   yeah = new Audio('yeah.wav');
 
+  playersTurn = true;
+
+  myInterval = null;
+
   shuffle = function(array) {
     var currentIndex, randomIndex, temporaryValue;
     currentIndex = array.length;
@@ -44,6 +47,10 @@
       array[randomIndex] = temporaryValue;
     }
     return array;
+  };
+
+  getRandom = function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
   };
 
   newList = shuffle(endList);
@@ -63,52 +70,79 @@
   };
 
   window.onload = function() {
-    var allCardsList, card, cards, k, len1, results;
+    var allCardsList, botPlay, k, len1;
     createCards();
     allCards = cardsContainer.querySelectorAll(".card");
+    for (k = 0, len1 = allCards.length; k < len1; k++) {
+      i = allCards[k];
+      i.classList.add("closed-card");
+    }
     allCardsList = Array.prototype.slice.call(allCards, 0);
     messageContainer.innerHTML = "Turns = " + turns;
-    cards = document.querySelectorAll(".card");
-    results = [];
-    for (k = 0, len1 = cards.length; k < len1; k++) {
-      card = cards[k];
-      results.push(card.addEventListener("click", function() {
-        var ind;
-        ind = allCardsList.indexOf(this);
+    botPlay = function() {
+      var botNum, closedCards, closedCardsList;
+      closedCards = document.querySelectorAll(".closed-card");
+      closedCardsList = Array.prototype.slice.call(closedCards, 0);
+      console.log(closedCardsList.length);
+      if (closedCards.length > 0) {
+        botNum = getRandom(0, closedCards.length);
+        closedCardsList[botNum].querySelector(".front").click();
+      }
+      return console.log(closedCards);
+    };
+    return cardsContainer.addEventListener("click", function(e) {
+      var card, ind;
+      if (e.target.parentNode.parentNode.parentNode.classList.contains("card")) {
+        card = e.target.parentNode.parentNode.parentNode;
+        ind = allCardsList.indexOf(card);
+        console.log(exposed[ind]);
         if (exposed[ind] === false) {
           exposed[ind] = true;
+          this.dataset.open = true;
           allCardsList[ind].querySelector(".flip-container").classList.add("animate");
+          allCardsList[ind].classList.remove("closed-card");
           blub.play();
           if (state === 0) {
             card1 = ind;
             state = 1;
             turns += 1;
-            messageContainer.innerHTML = "Turns = " + turns;
+            return messageContainer.innerHTML = "Turns = " + turns;
           } else if (state === 1) {
             card2 = ind;
-            state = 2;
-          } else {
+            state = 0;
             turns += 1;
             messageContainer.innerHTML = "Turns = " + turns;
             if (newList[card1] !== newList[card2]) {
               exposed[card1] = false;
               exposed[card2] = false;
-              allCardsList[card1].querySelector(".flip-container").classList.remove("animate");
-              allCardsList[card2].querySelector(".flip-container").classList.remove("animate");
-              card1 = ind;
-              state = 1;
-            } else {
-              card1 = ind;
-              state = 1;
+              exposed[ind] = false;
+              allCardsList[card1].dataset.open = false;
+              allCardsList[card2].dataset.open = false;
+              allCardsList[ind].dataset.open = false;
+              setTimeout(function() {
+                allCardsList[card1].querySelector(".flip-container").classList.remove("animate");
+                allCardsList[card2].querySelector(".flip-container").classList.remove("animate");
+                allCardsList[card1].classList.add("closed-card");
+                return allCardsList[card2].classList.add("closed-card");
+              }, 700);
+              if (playersTurn === true) {
+                playersTurn = false;
+                cardsContainer.classList.add("not-clickable");
+                return myInterval = setInterval(function() {
+                  if (!playersTurn) {
+                    return botPlay();
+                  }
+                }, 1000);
+              } else if (playersTurn === false) {
+                playersTurn = true;
+                cardsContainer.classList.remove("not-clickable");
+                return clearInterval(myInterval);
+              }
             }
           }
         }
-        if (!(indexOf.call(exposed, false) >= 0)) {
-          return yeah.play();
-        }
-      }));
-    }
-    return results;
+      }
+    }, false);
   };
 
 }).call(this);
--- coffee/script.coffee
+++ coffee/script.coffee
@@ -13,6 +13,8 @@ card2 = 33
 turns = 0
 blub = new Audio('blub.wav')
 yeah = new Audio('yeah.wav')
+playersTurn = true
+myInterval = null
 
 shuffle = (array) ->
   currentIndex = array.length
@@ -23,6 +25,9 @@ shuffle = (array) ->
     array[currentIndex] = array[randomIndex]
     array[randomIndex] = temporaryValue
   return array
+
+getRandom = (min, max) ->
+  return Math.floor(Math.random() * (max - min)) + min
 
 newList = shuffle(endList)
 
@@ -38,15 +43,31 @@ createCards = ->
 window.onload = ->
   createCards()
   allCards = cardsContainer.querySelectorAll(".card")
+  for i in allCards
+    i.classList.add("closed-card")
   allCardsList = Array.prototype.slice.call(allCards, 0)
   messageContainer.innerHTML = "Turns = " + turns
-  cards = document.querySelectorAll(".card")
-  for card in cards
-    card.addEventListener("click", ->
-      ind = allCardsList.indexOf(this)
+
+  # Bot
+  botPlay = ->
+    closedCards = document.querySelectorAll(".closed-card")
+    closedCardsList = Array.prototype.slice.call(closedCards, 0)
+    console.log closedCardsList.length
+    if closedCards.length > 0
+      botNum = getRandom(0, closedCards.length)
+      closedCardsList[botNum].querySelector(".front").click()
+    console.log closedCards
+
+  cardsContainer.addEventListener("click", (e)->
+    if e.target.parentNode.parentNode.parentNode.classList.contains("card")
+      card = e.target.parentNode.parentNode.parentNode
+      ind = allCardsList.indexOf(card)
+      console.log exposed[ind]
       if exposed[ind] == false
         exposed[ind] = true
+        this.dataset.open = true
         allCardsList[ind].querySelector(".flip-container").classList.add("animate")
+        allCardsList[ind].classList.remove("closed-card")
         blub.play()
         if state == 0
           card1 = ind
@@ -55,20 +76,31 @@ window.onload = ->
           messageContainer.innerHTML = "Turns = " + turns
         else if state == 1
           card2 = ind
-          state = 2
-        else
+          state = 0
           turns += 1
           messageContainer.innerHTML = "Turns = " + turns
           if newList[card1] != newList[card2]
             exposed[card1] = false
             exposed[card2] = false
-            allCardsList[card1].querySelector(".flip-container").classList.remove("animate")
-            allCardsList[card2].querySelector(".flip-container").classList.remove("animate")
-            card1 = ind
-            state = 1
-          else
-            card1 = ind
-            state = 1
-      if !(false in exposed)
-        yeah.play()
-    )
+            exposed[ind] = false
+            allCardsList[card1].dataset.open = false
+            allCardsList[card2].dataset.open = false
+            allCardsList[ind].dataset.open = false
+            setTimeout(->
+              allCardsList[card1].querySelector(".flip-container").classList.remove("animate")
+              allCardsList[card2].querySelector(".flip-container").classList.remove("animate")
+              allCardsList[card1].classList.add("closed-card")
+              allCardsList[card2].classList.add("closed-card")
+            , 700)
+            if playersTurn == true
+              playersTurn = false
+              cardsContainer.classList.add("not-clickable")
+              myInterval = setInterval(->
+                if !playersTurn
+                  botPlay()
+              , 1000)
+            else if playersTurn == false
+              playersTurn = true
+              cardsContainer.classList.remove("not-clickable")
+              clearInterval(myInterval)
+  , false)
